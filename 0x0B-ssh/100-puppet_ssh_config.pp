7c64ab05e5482a089cd12939f0b9b3de45fcadc2
StellarTeshiBlank <blankteshi079@gmail.com>
2024-09-26 13:51:08 +0100

    Configuring puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env /bash
+# Using and Understanding puppet to make changes to our configuration
+
+file { 'etc/ssh/ssh_config':
+	ensure => present, 
+
+content =>"
+
+	#SSH client configuration 
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
