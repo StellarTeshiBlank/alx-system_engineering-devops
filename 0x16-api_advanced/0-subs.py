0135a76705d8b6031bedf9f89b3471fe94ba57ad
StellarTeshiBlank <blankteshi079@gmail.com>
2024-09-26 09:04:05 +0100

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,23 +8,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of
-    subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers, or 0 if the
-        subreddit is invalid or doesn't exist.
+    Queries the Reddit API and returns
+    the number of subscribers for a subreddit
     """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    url = f"https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-
-    data = response.json().get('data', {})
-    return data.get('subscribers', 0)
