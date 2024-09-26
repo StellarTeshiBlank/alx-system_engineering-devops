36f5e02634e67a52d48b01fc0f04c9036777a9ba
StellarTeshiBlank <blankteshi079@gmail.com>
2024-09-26 13:55:22 +0100

    Configuring puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,8 @@
 #!/usr/bin/env /bash
 # Using and Understanding puppet to make changes to our configuration
 
-file { 'etc/ssh/ssh_config':
-	ensure => present, 
-
-content =>"
-
-	#SSH client configuration 
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	"
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
 }
